a0e7b52a7180970c376d6e52dfc2d087e8dc247e
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-22 23:59:49 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -18,12 +18,21 @@ var positions = [
     }
 ];
 
+var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+
 for (var i = 0; i < positions.length; i ++) {
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
-        position: positions[i].latlng // 마커의 위치
+        position: positions[i].latlng, // 마커의 위치
+        image: markerImage // 마커이미지 설정
     });
+    
 
     // 마커에 표시할 인포윈도우를 생성합니다 
     var infowindow = new kakao.maps.InfoWindow({
