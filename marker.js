4a85a7db9230fb0c8d88992da3db65d55e6fbb59
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-22 23:07:24 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -32,3 +32,15 @@ kakao.maps.event.addListener(marker, 'click', function() {
       // 마커 위에 인포윈도우를 표시합니다
       infowindow.open(map, marker);  
 });
+
+// 마커에 마우스오버 이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'mouseover', function() {
+  // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+    infowindow.open(map, marker);
+});
+
+// 마커에 마우스아웃 이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'mouseout', function() {
+    // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+    infowindow.close();
+});
