18de5ed48c89127a93be1e5157e658bdddd7f8b1
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-23 00:07:24 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -18,7 +18,7 @@ var positions = [
     }
 ];
 
-var imageSrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIuVC2S4Z0Pdzrk-OfaU-3wDQz8W-bayDXWQ&usqp=CAU', // 마커이미지의 주소입니다    
+var imageSrc = 'https://www.pngmart.com/files/7/Delivery-PNG-Picture.png', // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(60, 64), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
