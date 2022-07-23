3288f5117c6bcdee170d5fb665decdacde606a12
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-23 10:47:08 +0900

    Update library.js

--- library.js
+++ library.js
@@ -5,17 +5,32 @@
         minLevel: 10 // 클러스터 할 최소 지도 레벨 
     });
  
-    // 데이터를 가져오기 위해 jQuery를 사용합니다
-    // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-    $.get("/download/web/data/chicken.json", function(data) {
-        // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-        // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-        var markers = $(data.positions).map(function(i, position) {
-            return new kakao.maps.Marker({
-                position : new kakao.maps.LatLng(position.lat, position.lng)
-            });
-        });
+   var data = {
+  "positions": [
+    {
+      "lat": 37.27943075229118,
+      "lng": 127.01763998406159
+    },
+    {
+      "lat": 37.55915668706214,
+      "lng": 126.92536526611102
+    },
+    {
+      "lat": 35.13854258261161,
+      "lng": 129.1014781294671
+    },
+    {
+      "lat": 37.55518388656961,
+      "lng": 126.92926237742505
+    },
+    /* ...... */
+};
 
-        // 클러스터러에 마커들을 추가합니다
-        clusterer.addMarkers(markers);
+var markers = data.positions.map(function(position) {
+    return new kakao.maps.Marker({
+        position : new kakao.maps.LatLng(position.lat, position.lng)
     });
+});
+
+// 클러스터러에 마커들을 추가합니다
+clusterer.addMarkers(markers);
