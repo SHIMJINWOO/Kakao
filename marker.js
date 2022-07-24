9db89ae54ff7d05dd21c848269f7a5069fe27d17
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-24 21:08:35 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -33,12 +33,6 @@ for (var i = 0; i < positions.length; i ++) {
         image: markerImage // 마커이미지 설정
     });
     
-
-    
-
-// 클러스터러에 마커들을 추가합니다
-clusterer.addMarkers(markers);
-    
     // 마커에 표시할 인포윈도우를 생성합니다 
     var infowindow = new kakao.maps.InfoWindow({
         content: positions[i].content // 인포윈도우에 표시할 내용
@@ -78,8 +72,9 @@ var clusterer = new kakao.maps.MarkerClusterer({
 var data = {
   "positions": [
     {
-      "lat": 37.27943075229118,
-      "lng": 127.01763998406159
+      "content": '<div>본사 <p> ㅌㅌ</div>', 
+      "lat": 37.54699,
+      "lng": 127.09598
     },
     {
       "lat": 37.55915668706214,
@@ -583,12 +578,33 @@ var data = {
     }
   ]
 }
+var imageSrc = 'https://www.pngmart.com/files/7/Delivery-PNG-Picture.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(60, 64), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
 
 var markers = data.positions.map(function(position) {
     return new kakao.maps.Marker({
-        position : new kakao.maps.LatLng(position.lat, position.lng)
+    position : new kakao.maps.LatLng(position.lat, position.lng),
+    image: markerImage // 마커이미지 설정
     });
 });
+
+
+
+/*
+
+for (var i = 0; i < positions.length; i ++) {
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커의 위치
+        image: markerImage // 마커이미지 설정
+    });
+
+*/ 
 
 // 클러스터러에 마커들을 추가합니다
 clusterer.addMarkers(markers);
