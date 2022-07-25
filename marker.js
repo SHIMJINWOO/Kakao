60db9159ce0e7ea53e1f23b11dd56c3ced1343f4
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-25 17:37:07 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -155,15 +155,14 @@ var markers = data.positions.map(function(position) {
     });
 });
 
-
-var iwContent = '<div style="padding:5px;">Hello World! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    iwPosition = new kakao.maps.LatLng(37.54699,127.09598); //인포윈도우 표시 위치입니다
-
+for (var i=0; i<data.positions.length; i++){
+const iwContent = data.positions[i].content
+const iwPosition = new kakao.maps.LatLng(`${data.positions[i].lat}`,`${data.positions[i].lng}`)
 // 인포윈도우를 생성합니다
 var infowindow = new kakao.maps.InfoWindow({
     position : iwPosition, 
     content : iwContent 
-});
+});}
   
 // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
 infowindow.open(map); 
