c8d8b6154b610e7d8b99924a9cd8abf24f4c2940
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-25 18:07:28 +0900

    Update infoWindow.js

--- infoWindow.js
+++ infoWindow.js
@@ -128,13 +128,23 @@ var positions = [
 {content: '<div>본사 <p> ㅌㅌ</div>',latlng: new kakao.maps.LatLng(35.5272027005698, 127.729537989501)},
 {content: '<div>본사 <p> ㅌㅌ</div>',latlng: new kakao.maps.LatLng(35.1800322858988, 128.069545091753)}
                 ];
+  
+var imageSrc = 'MicrosoftTeams-image (8).png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(0, 0), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+
+
 
 
 for (var i = 0; i < positions.length; i ++) {
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
-        position: positions[i].latlng // 마커의 위치
+        position: positions[i].latlng, // 마커의 위치
+        image: markerImage
     });
 
     // 마커에 표시할 인포윈도우를 생성합니다 
@@ -162,3 +172,4 @@ function makeOutListener(infowindow) {
         infowindow.close();
     };
 }
+
