24b827fde485501bf5b5dcb18fe0653b51bfebda
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-25 18:09:58 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -141,7 +141,7 @@ var data = {"positions":
 
 
   
-var imageSrc = 'MicrosoftTeams-image (8).png', // 마커이미지의 주소입니다    
+var imageSrc = None, // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(0, 0), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
@@ -153,6 +153,7 @@ var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
 var markers = data.positions.map(function(position) {
     return new kakao.maps.Marker({
     position : new kakao.maps.LatLng(position.lat, position.lng),
+    image : makerImage
     });
 });
 
