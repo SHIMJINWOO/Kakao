3b304382e0dfe866f106405f8fda67c9aff4fd25
SHIMJINWOO <108581374+SHIMJINWOO@users.noreply.github.com>
2022-07-25 18:14:19 +0900

    Update marker.js

--- marker.js
+++ marker.js
@@ -141,7 +141,7 @@ var data = {"positions":
 
 
   
-var imageSrc = 'https://www.pngfind.com/pngs/m/64-646976_nothing-png-sky-transparent-png.png', // 마커이미지의 주소입니다    
+var // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(0, 0), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
